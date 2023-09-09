1094a030895de78188c38ebf813e3a28df515aae
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2023-09-09 17:36:01 +0800

    configure sequelize

---
+++ config/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    development: {
+        username: process.env.LOCAL_DB_USERNAME,
+        password: process.env.LOCAL_DB_PASSWORD,
+        database: process.env.LOCAL_DB_DATABASE,
+        host: 'localhost',
+        dialect: 'postgres',
+    },
+    docker: {
+        username: process.env.DOCKER_DB_USERNAME,
+        password: process.env.DOCKER_DB_PASSWORD,
+        database: process.env.DOCKER_DB_DATABASE,
+        host: process.env.DOCKER_DB_HOST,
+        port: process.env.DOCKER_DB_PORT,
+        dialect: 'postgres',
+    },
+    test: {
+        username: 'root',
+        password: null,
+        database: 'database_test',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    production: {
+        username: 'root',
+        password: null,
+        database: 'database_production',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+};
