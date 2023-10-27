bfb23e8c7173d925d1e79d09708fe4c3f75ce7d6
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2023-10-27 09:02:35 +0800

    DMA-17 | Add authorization to adminMonthlyDutySchedule endpoint

--- src/routes/monthlyDutySchedule/adminMonthlyDutySchedule.router.ts
+++ src/routes/monthlyDutySchedule/adminMonthlyDutySchedule.router.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import AdminMonthlyDutyScheduleController from '../../controllers/monthlyDutySchedule/monthlyDutySchedule.admin.controller';
+import AuthMiddleware from '../../controllers/auth/authMiddleware.controller';
+import { UserAccountType } from '../../models/userAccount/userAccount.enum';
 
 const adminMonthlyDutyScheduleRouter = Router();
+
+adminMonthlyDutyScheduleRouter.use(
+    AuthMiddleware.authorized([UserAccountType.ADMIN]),
+);
 
 adminMonthlyDutyScheduleRouter.get(
     '/',
