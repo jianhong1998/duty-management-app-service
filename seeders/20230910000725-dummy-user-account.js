fd80df7122dbeffe783b919116ee9dae5b7df842
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2023-12-09 18:52:06 +0800

    Staging | Change admin name and all password

--- seeders/20230910000725-dummy-user-account.js
+++ seeders/20230910000725-dummy-user-account.js
@@ -17,7 +17,7 @@ module.exports = {
                 employee_id: 1,
                 email_address: 'admin@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -25,7 +25,7 @@ module.exports = {
                 employee_id: 2,
                 email_address: 'user@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -33,7 +33,7 @@ module.exports = {
                 employee_id: 3,
                 email_address: 'velit@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -41,7 +41,7 @@ module.exports = {
                 employee_id: 4,
                 email_address: 'scelerisque@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -49,7 +49,7 @@ module.exports = {
                 employee_id: 5,
                 email_address: 'ipsum.phasellus@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -57,7 +57,7 @@ module.exports = {
                 employee_id: 6,
                 email_address: 'natoque.penatibus.et@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -65,7 +65,7 @@ module.exports = {
                 employee_id: 7,
                 email_address: 'consectetuer.mauris@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -73,7 +73,7 @@ module.exports = {
                 employee_id: 8,
                 email_address: 'et@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -81,7 +81,7 @@ module.exports = {
                 employee_id: 9,
                 email_address: 'per.conubia.nostra@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -89,7 +89,7 @@ module.exports = {
                 employee_id: 10,
                 email_address: 'phasellus.dapibus@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -97,7 +97,7 @@ module.exports = {
                 employee_id: 11,
                 email_address: 'porttitor@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -105,7 +105,7 @@ module.exports = {
                 employee_id: 12,
                 email_address: 'tortor.integer@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -113,7 +113,7 @@ module.exports = {
                 employee_id: 13,
                 email_address: 'ultrices.posuere@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -121,7 +121,7 @@ module.exports = {
                 employee_id: 14,
                 email_address: 'dictum@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -129,7 +129,7 @@ module.exports = {
                 employee_id: 15,
                 email_address: 'aliquam.ultrices@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -137,7 +137,7 @@ module.exports = {
                 employee_id: 16,
                 email_address: 'ligula.aenean@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -145,7 +145,7 @@ module.exports = {
                 employee_id: 17,
                 email_address: 'felis.adipiscing@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -153,7 +153,7 @@ module.exports = {
                 employee_id: 18,
                 email_address: 'mauris.molestie.pharetra@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -161,7 +161,7 @@ module.exports = {
                 employee_id: 19,
                 email_address: 'ut@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Active',
             },
@@ -169,7 +169,7 @@ module.exports = {
                 employee_id: 20,
                 email_address: 'praesent.eu.dui@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Active',
             },
@@ -177,7 +177,7 @@ module.exports = {
                 employee_id: 21,
                 email_address: 'nulla.cras@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Disabled',
             },
@@ -185,7 +185,7 @@ module.exports = {
                 employee_id: 22,
                 email_address: 'ut.sem.nulla@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Disabled',
             },
@@ -193,7 +193,7 @@ module.exports = {
                 employee_id: 23,
                 email_address: 'ornare.elit.elit@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Disabled',
             },
@@ -201,7 +201,7 @@ module.exports = {
                 employee_id: 24,
                 email_address: 'eleifend.vitae.erat@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Disabled',
             },
@@ -209,7 +209,7 @@ module.exports = {
                 employee_id: 25,
                 email_address: 'nisi.aenean.eget@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Disabled',
             },
@@ -217,7 +217,7 @@ module.exports = {
                 employee_id: 26,
                 email_address: 'dictum.placerat@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Disabled',
             },
@@ -225,7 +225,7 @@ module.exports = {
                 employee_id: 27,
                 email_address: 'sem.magna@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Disabled',
             },
@@ -233,7 +233,7 @@ module.exports = {
                 employee_id: 28,
                 email_address: 'natoque@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'User',
                 account_status: 'Disabled',
             },
@@ -241,7 +241,7 @@ module.exports = {
                 employee_id: 29,
                 email_address: 'quis.urna@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Disabled',
             },
@@ -249,7 +249,7 @@ module.exports = {
                 employee_id: 30,
                 email_address: 'pellentesque.habitant@test.com',
                 password:
-                    '$2a$15$Hgu9t4HDixhNih.mLa3lVO9z.wmPTyIFN6A9xyIkmfuPk2/vAuMea',
+                    '$2b$10$PQFdIjj0NIlTeZVCQM0ZOO4huRTQtv0pBGVAVpRDwULV/MxG0u9Ri',
                 account_type: 'Admin',
                 account_status: 'Disabled',
             },
